5c1376fc51ff4aacd9ed086311d5569810028ef0
Martin Simeonov <i525452@fhict.nl>
2024-09-16 19:33:39 +0200

    Initial set up to tha api

---
+++ src/main/java/torquehub/torquehub/TorqueHubApplication.java
@@ -0,0 +1,13 @@
+package torquehub.torquehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorqueHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TorqueHubApplication.class, args);
+    }
+
+}
